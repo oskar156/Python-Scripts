f4d7950c5a83df87925714254beaf745e8c84d99
Oscar Keshishyan <89118890+oskar156@users.noreply.github.com>
2023-11-21 15:57:00 -0800

    Add files via upload

---
+++ regex_find_and_replace_string_for_each_file_in_folder.py
@@ -0,0 +1,23 @@
+#run this python file in the directory where you'd like to perform find-and-replace on the files
+
+#from https://stackoverflow.com/questions/50037369/find-and-replace-string-from-multiple-files-in-a-folder-using-python
+#from https://stackoverflow.com/questions/11475885/python-replace-regex
+
+import glob
+import ntpath
+import os
+import re
+
+output_dir = r'C:\Users\oscar\Desktop'
+file_type = "*.html"
+find_regex_exp = 'find'
+replacement_str = 'replace'
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+for f in glob.glob(file_type):
+    with open(f, 'r') as inputfile:
+        with open('%s/%s' % (output_dir, ntpath.basename(f)), 'w') as outputfile:
+            for line in inputfile:
+                outputfile.write(re.sub(find_regex_exp, replacement_str, line))
