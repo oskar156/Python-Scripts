b0cdd0431dfe0893f9ca8d9e268a3da87c0e8a26
Oscar Keshishyan <89118890+oskar156@users.noreply.github.com>
2025-01-14 15:50:09 -0800

    Add files via upload

---
+++ RENAME FILES IN FOLDER (APPEND PREFIX OR SUFFIX).py
@@ -0,0 +1,17 @@
+import os
+
+prefix = 'GPPO'
+suffix = ''
+
+path = os.getcwd() #CURRENT DIRECTORY
+script_file_path = input(__file__)
+
+i = 0
+for file in os.listdir(path):
+  if file != script_file_path:
+    og_file_name = os.path.basename(file)
+    new_file_name = str(prefix) + str(og_file_name) + str(suffix)
+    os.rename(os.path.join(path, og_file_name), os.path.join(path, new_file_name))
+    print(str(i + 1) + ": " + str(os.path.join(path, og_file_name)) + " renamed to " + str(os.path.join(path, new_file_name)))
+    i = i + 1
+  
